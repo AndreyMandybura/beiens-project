396a750e63308954a5e05d6331c0aaaea9d72ede
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-07-07 12:56:38 +0300

    Добавил responsive-carousel, поправил стили

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,23 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from 'next/image';
+import { Carousel } from 'react-responsive-carousel';
+import Img1 from '../../images/carouselImages/Img1.jpg'; 
+import Img2 from '../../images/carouselImages/Img2.jpg'; 
+import Img3 from '../../images/carouselImages/Img3.jpg'; 
+
+export function ImageCarousel() {
+
+    return (
+        <Carousel autoPlay={true} interval={3000} infiniteLoop={true} showStatus={false} showThumbs={true}>
+            <div>
+                <Image src={Img1} alt="" />
+            </div>
+            <div>
+                <Image src={Img2} alt="" />
+            </div>
+            <div>
+                <Image src={Img3} alt="" />
+            </div>
+        </Carousel>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { NavBar } from "../components/NavBar/NavBar";
-import { Slider } from "../components/Slider/Slider";
+import { ImageCarousel } from "../components/Carousel/Carousel";
 
 export default function Home() {
     return (
         <>
             <NavBar title={"Головна"}>
                 <h1>Main</h1>
-                <Slider />
+                <ImageCarousel />
             </NavBar>
             
         </>
