c6406d6aa7ca93490f0da592c435088410b8341a
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-07-22 13:50:20 +0300

    Links for Trendid items

--- components/Trendind/Trendind.js
+++ components/Trendind/Trendind.js
@@ -5,6 +5,7 @@ import rattles from '../../images/trendidImages/YZ42-6.jpg';
 import pens from '../../images/trendidImages/ZN17-3.jpg';
 import babyRugs from '../../images/trendidImages/B250-1.jpg';
 import tablets from '../../images/trendidImages/ZJ16-C-2.jpg';
+import Link from 'next/link';
 
 export function Trendind() {
 
@@ -13,50 +14,70 @@ export function Trendind() {
             <h2 className={s.titleText}>популярне</h2>
             <ul className={s.trendidThumb}>
                 <li className={s.trendidItem}>
-                    <div className={s.trendidImg}>
-                        <Image src={forBabies} 
-                            layout="fill"
-                            objectFit="cover"
-                            alt="child with a toy" />
-                    </div>
+                    <Link href={'/'}>
+                        <a>
+                            <div className={s.trendidImg}>
+                                <Image src={forBabies} 
+                                layout="fill"
+                                objectFit="cover"
+                                alt="child with a toy" />
+                            </div>
+                        </a>
+                    </Link>
                     <p className={s.cardTitle}>для малюків</p>
                 </li>
                 <li className={s.trendidItem}>
-                    <div className={s.trendidImg}>
-                        <Image src={rattles} 
-                            layout="fill"
-                            objectFit="cover"
-                            alt="child with a toys" />
-                    </div>
+                    <Link href={'/'}>
+                        <a>
+                            <div className={s.trendidImg}>
+                                <Image src={rattles} 
+                                layout="fill"
+                                objectFit="cover"
+                                alt="child with a toys" />
+                            </div>
+                        </a>
+                    </Link>
                     <p className={s.cardTitle}>брязкальця та гризунці</p>
                 </li>
             </ul>
             <ul className={s.trendidThumb}>
                 <li className={s.trendidItem}>
-                    <div className={s.trendidImgSL}>
-                        <Image src={pens}
-                            layout="fill"
-                            objectFit="cover"
-                            alt="mother playing with child" />
-                    </div>
+                    <Link href={'/'}>
+                        <a>
+                            <div className={s.trendidImgSL}>
+                                <Image src={pens}
+                                layout="fill"
+                                objectFit="cover"
+                                alt="mother playing with child" />
+                            </div>
+                        </a>
+                    </Link>
                     <p className={s.cardTitle}>3D ручки</p>
                 </li>
                 <li className={s.trendidItem}>
-                    <div className={s.trendidImgSL}>
-                        <Image src={babyRugs} 
-                            layout="fill"
-                            objectFit="cover"
-                            alt="child playing on a children's rug" />
-                    </div>
+                    <Link href={'/'}>
+                        <a>
+                            <div className={s.trendidImgSL}>
+                                <Image src={babyRugs} 
+                                layout="fill"
+                                objectFit="cover"
+                                alt="child playing on a children's rug" />
+                            </div>
+                        </a>
+                    </Link>
                     <p className={s.cardTitle}>розвиваючі центри</p>
                 </li>
                 <li className={s.trendidItem}>
-                    <div className={s.trendidImgSL}>
-                        <Image src={tablets} 
-                            layout="fill"
-                            objectFit="cover"
-                            alt="child with tablet" />
-                    </div>
+                    <Link href={'/'}>
+                        <a>
+                            <div className={s.trendidImgSL}>
+                                <Image src={tablets} 
+                                layout="fill"
+                                objectFit="cover"
+                                alt="child with tablet" />
+                            </div>
+                        </a>
+                    </Link>
                     <p className={s.cardTitle}>графічні планшети</p>
                 </li>
             </ul>
