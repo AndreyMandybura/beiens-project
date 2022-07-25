1720549da46ea01265074327ddc1119f488a8a82
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-07-25 18:14:45 +0300

    Brand component

--- pages/brand.js
+++ pages/brand.js
@@ -1,10 +1,13 @@
+import { BrandStory } from "../components/BrandStory/BrandStory";
+import { Footer } from "../components/Footer/Footer";
 import { NavBar } from "../components/NavBar/NavBar";
 
 export default function Brand() {
     return (
         <>
             <NavBar title={"Про бренд"}>
-                <h1>Brand</h1>
+                <BrandStory />
+                <Footer />
             </NavBar>
         </>
     )
