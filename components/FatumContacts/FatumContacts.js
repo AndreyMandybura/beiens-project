bbc9fc13f34ab65272df602888ad694df8f848e6
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-07-29 13:08:25 +0300

    Update NavBar mobile menu

--- components/FatumContacts/FatumContacts.js
+++ components/FatumContacts/FatumContacts.js
@@ -28,7 +28,7 @@ export function FatumContacts() {
                 </ul>
                 <h3 className={s.contactsTitle}>Адреса</h3>
                 <p className={s.contactsItem}>м. Київ, вул. Нагірна, 22, офіс 332</p>
-                <iframe className={s.googleMap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.340929912655!2d30.481534515899835!3d50.47199649369294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cddf4222858d%3A0x3d120be38ebe5356!2z0YPQuy4g0J3QsNCz0L7RgNC90LDRjywgMjIsINCa0LjQtdCyLCAwMjAwMA!5e0!3m2!1sru!2sua!4v1658829969184!5m2!1sru!2sua" style={{border:0}} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe className={s.googleMap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2539.340929912655!2d30.481534515899835!3d50.47199649369294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cddf4222858d%3A0x3d120be38ebe5356!2z0YPQuy4g0J3QsNCz0L7RgNC90LDRjywgMjIsINCa0LjQtdCyLCAwMjAwMA!5e0!3m2!1sru!2sua!4v1658829969184!5m2!1sru!2sua" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </>
     );
--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -19,32 +19,27 @@ export function NavBar({ children, title='Beiens' }) {
                 <meta name="description" content="This is the official website of children's toys brand Beiens in Ukraine" />
             </Head>
             <nav className="container">
-                <div className={s.navContainer}>
-                    <div className={s.navWrapper}>
-                        <Link href={'/'}><a className={s.logo} ><Image src={logo} alt="logo" width={150} height={46} /></a></Link>
-                            <ul className={s.navList}>
-                                <li className={s.navItem}><Link href={'/'}><a>головна</a></Link></li>
-                                <li className={s.navItem}><Link href={'/catalog'}><a>каталог</a></Link></li>
-                                <li className={s.navItem}><Link href={'/brand'}><a>про бренд</a></Link></li>
-                                <li className={s.navItem}><Link href={'/gallery'}><a>галерея</a></Link></li>
-                                <li className={s.navItem}><Link href={'/contacts'}><a>контакти</a></Link></li>
-                            </ul>
-                        <button type="button" className={s.menuButton} onClick={() => setActive(!active)}>
-                            <svg width={40} height={40} aria-label="Mobile menu switch" className={active ? s.iconCross : s.iconMenu}>
-                                <CloseBtn className={s.iconCross} />
-                                <MenuIcon className={s.iconMenu} />
-                            </svg>
-                        </button>
+                <div className={active ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset'} >
+                    <div className={s.navContainer}>
+                        <div className={s.navWrapper}>
+                            <Link href={'/'}><a className={s.logo} ><Image src={logo} alt="logo" width={150} height={46} /></a></Link>
+                            <div className={active ? s.activeNavListContainer : s.navListContainer}>
+                                <ul className={s.navList}>
+                                    <li className={s.navItem}><Link href={'/'}><a>головна</a></Link></li>
+                                    <li className={s.navItem}><Link href={'/catalog'}><a>каталог</a></Link></li>
+                                    <li className={s.navItem}><Link href={'/brand'}><a>про бренд</a></Link></li>
+                                    <li className={s.navItem}><Link href={'/gallery'}><a>галерея</a></Link></li>
+                                    <li className={s.navItem}><Link href={'/contacts'}><a>контакти</a></Link></li>
+                                </ul>
+                            </div>
+                            <button type="button" className={s.menuButton} onClick={() => setActive(!active)}>
+                                <svg width={40} height={40} aria-label="Mobile menu switch" className={active ? s.iconCross : s.iconMenu}>
+                                    <CloseBtn className={s.iconCross} />
+                                    <MenuIcon className={s.iconMenu} />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className={active ? s.activeSideNav : s.sideNav}>
-                    <ul className={s.sideNavList}>
-                        <li className={s.sideNavItem}><Link href={'/'}><a>головна</a></Link></li>
-                        <li className={s.sideNavItem}><Link href={'/catalog'}><a>каталог</a></Link></li>
-                        <li className={s.sideNavItem}><Link href={'/brand'}><a>про бренд</a></Link></li>
-                        <li className={s.sideNavItem}><Link href={'/gallery'}><a>галерея</a></Link></li>
-                        <li className={s.sideNavItem}><Link href={'/contacts'}><a>контакти</a></Link></li>
-                    </ul>
                 </div>
             </nav>
             <main className="container">
